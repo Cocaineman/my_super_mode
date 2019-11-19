d04ea5422c14c209d3d54523fde900e28ece75dd
nrnkd <minecrafter925@yandex.ru>
2019-11-19 18:14:13 +0300

    `super update 111`

--- java/com/example/examplemod/ExampleMod.java
+++ java/com/example/examplemod/ExampleMod.java
@@ -12,7 +12,7 @@ public class ExampleMod
 {
     public static final String MODID = "mysupermod";
     public static final String NAME = "My Super Mod";
-    public static final String VERSION = "1.228432423";
+    public static final String VERSION = "1.23";
 
     private static Logger logger;
 
